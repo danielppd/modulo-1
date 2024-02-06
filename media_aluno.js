a18a3afd9199cbcd44c6aeefb0c91ea6da8d98fa
danielppd <daniel.dias@sou.inteli.edu.br>
2024-02-06 15:58:17 -0300

    Ponderada dia 06/02
    atividade ponderada média de aluno

---
+++ media_aluno.js
@@ -0,0 +1,11 @@
+var n1 = 9
+var n2 = 8
+var media = (n1 + n2) / 2 
+console.log("A primeira nota do aluno foi", n1)
+console.log("A segunda nota do aluno foi", n2)
+
+if (media >= 6) {
+    console.log("Então, a média do aluno foi", media, "e ele foi Aprovado!");
+} else{
+    console.log("Então, a média do aluno foi", media, "e ele foi Reprovado :(")
+}
